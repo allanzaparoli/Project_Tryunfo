8eb8996fbc2986b9af1e1e428601823405f2fd9c
allanzaparoli <allanzaparoli@gmail.com>
2022-04-18 23:06:22 -0300

    arrumando erros do lint

--- src/components/Card.js
+++ src/components/Card.js
@@ -33,12 +33,14 @@ class Card extends Component {
 Card.propTypes = {
   cardName: PropTypes.string.isRequired,
   cardDescription: PropTypes.string.isRequired,
-  cardAttr1: PropTypes.string.isRequired,
-  cardAttr2: PropTypes.string.isRequired,
-  cardAttr3: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  cardAttr2: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  cardAttr3: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
-  cardTrunfo: PropTypes.bool.isRequired,
+  cardTrunfo: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
 };
-
+Card.defaultProps = {
+  cardTrunfo: false,
+};
 export default Card;
